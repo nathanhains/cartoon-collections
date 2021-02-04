e35da2d8f4d7a094eba80417862c59d65147f97e
nathan hains <nathanhains14@yahoo.com>
2021-02-04 00:15:00 -0500

    commit tree cd5a9fbbbedf8c9b5fd30405db49d54920713b90

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  i = 0
+  while i < dwarves.length
+    print "#{i+1}. #{dwarves[i]}"
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)# code an argument here
+  calls.map {|n| "#{n.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? {|i| i.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect {|i| i == "cheddar" || i == "gouda"|| i == "camembert"}
+
 end
